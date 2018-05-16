181592d03f0fe2eb35f5747c8c1643089dc87635
ZR <zuhdi.robbani@gmail.com>
2018-05-16 14:24:50 +0700

    user

---
+++ application/controllers/administrator/User.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class User extends CI_Controller {
+	public function __construct(){
+		parent::__construct();
+		$this->load->model('mpage');
+		if($this->session->userdata('login_user') == FALSE){
+			redirect(base_url('administrator/dashboard/login/'));
+		}
+	}
+
+	public function manage_user(){
+		$data['path_content'] = 'admin/user/manage-user';
+		if($this->session->userdata('login_user') == FALSE){
+			redirect(base_url('administrator/dashboard/login/'));
+		}
+		$this->load->view('admin/dashboard', $data);
+	}
+}
