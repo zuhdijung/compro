83ebebc246992714ae934e253e482b220717e12a
ZR <zuhdi.robbani@gmail.com>
2018-05-17 11:27:23 +0700

    template

---
+++ application/views/admin/user/add-user.php
@@ -0,0 +1,31 @@
+
+
+
+        <div class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="header">
+                                <h4 class="title">Add User</h4>
+                                <div style="margin-top:25px">
+                                        <a href="<?php echo base_url('administrator/user/manage')?>"><button type="submit" class="btn btn-info btn-fill btn-wd">Manage User</button>
+                                    </div></a>
+                                
+                            </div>
+                            <div class="content table-responsive table-full-width">
+                                
+
+                            </div>
+                        </div>
+                    </div>
+
+
+                
+
+
+                </div>
+            </div>
+        </div>
+
+       
