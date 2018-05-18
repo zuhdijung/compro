9414bc90bbb68ddbdc7b2aa1ae288b0b5ae6fc74
Rani Putri Merliasari <rpmhslg@gmail.com>
2018-05-18 10:40:07 +0700

    delete notify

--- application/views/admin/common/script.php
+++ application/views/admin/common/script.php
@@ -25,14 +25,7 @@
 
             demo.initChartist();
 
-            $.notify({
-                icon: 'ti-gift',
-                message: "Welcome to <b>Paper Dashboard</b> - a beautiful Bootstrap freebie for your next project."
-
-            },{
-                type: 'success',
-                timer: 4000
-            });
+            
 
         });
     </script>
