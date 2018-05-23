9b969a79b46c46983408431c66cef2e122b25262
Rani Putri Merliasari <rpmhslg@gmail.com>
2018-05-23 16:50:01 +0700

    update category dan page

--- application/models/Marticle.php
+++ application/models/Marticle.php
@@ -23,4 +23,38 @@ class Marticle extends CI_Model {
 	    }
 	    else return FALSE;
 	}
+	function countCategory(){
+		return $this->db->count_all_results('category');
+	}
+
+	function fetchCategory($limit,$start,$pagenumber) {
+	    if($pagenumber!="")
+	      $this->db->limit($limit,($pagenumber*$limit)-$limit);
+	    else
+	      $this->db->limit($limit,$start);
+	    $this->db->order_by('id_category','DESC');
+	    
+	    $query = $this->db->get('category');
+	    if($query->num_rows()>0){
+	      return $query->result();
+	    }
+	    else return FALSE;
+	}
+	function countPage(){
+		return $this->db->count_all_results('page');
+	}
+
+	function fetchPage($limit,$start,$pagenumber) {
+	    if($pagenumber!="")
+	      $this->db->limit($limit,($pagenumber*$limit)-$limit);
+	    else
+	      $this->db->limit($limit,$start);
+	    $this->db->order_by('id_page','DESC');
+	    
+	    $query = $this->db->get('page');
+	    if($query->num_rows()>0){
+	      return $query->result();
+	    }
+	    else return FALSE;
+	}
 }
